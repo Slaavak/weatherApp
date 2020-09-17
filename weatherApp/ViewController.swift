8c92f720a82e39ceeb1172b695392cb33b027f22
Slavak <slavak1998@gmail.com>
2020-09-17 14:58:37 +0300

    first commit

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Slava Korolevich on 9/17/20.
+//  Copyright © 2020 Slava Korolevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
